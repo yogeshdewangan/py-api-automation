e5a64b7c9f562d43dbd25f7b1c1b05e62988c74b
Yogesh <yogeshdewangan97@gmail.com>
2022-03-20 14:59:38 +0530

    send email

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,7 @@ def pytest_addoption(parser):
     parser.addoption("--testenv", action="store", default="DEFAULT")
 
 def pytest_sessionfinish(session, exitstatus):
-    receiver_emails =  props["reciever_emails"]
+    receiver_emails =  props["receiver_emails"]
     sender_email = props["sender_email"]
     sender_password = props["sender_password"]
     subject = "Html report of pytest pipeline"
