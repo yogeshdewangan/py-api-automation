9e1919e236c9e89b8ca6cf52ae39a8ca35f4e70d
Yogesh <yogeshdewangan97@gmail.com>
2022-03-20 13:57:07 +0530

    send email

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,32 @@
 import pytest
 import os
 import logging
+import utilities.send_email as emailto
+from configuration.getconfig import props
 log = logging.getLogger(__name__)
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 input_file_path = os.path.join(ROOT_DIR, "inputs\data.csv").replace("tests\\", "")
-
-# @pytest.fixture(scope="class")
-# def setup(request):
-#     # Read command line parameter and put it in environment variable
-#     os.environ['CONF'] = request.config.getoption("testenv", "DEFAULT")
-#
-#     log.debug("setup")
-#     yield
-#     log.debug("teardown")
 
 
 @pytest.fixture(scope="function")
 def setup(request):
     print("setup_test called")
-    # try:
-    #     output_file_path = os.path.join(ROOT_DIR, "outputs\data.csv").replace("tests\\", "")
-    #     os.remove(output_file_path)
-    # except:
-    #     pass
-
-
+    try:
+        output_file_path = os.path.join(ROOT_DIR, "outputs\data.csv").replace("tests\\", "")
+        os.remove(output_file_path)
+    except:
+        pass
     yield
+
     print("teardown called")
 
 def pytest_addoption(parser):
     parser.addoption("--testenv", action="store", default="DEFAULT")
 
-@pytest.fixture()
-def read_inputfile(self):
-
-    return input
+def pytest_sessionfinish(session, exitstatus):
+    receiver_emails =  props["reciever_emails"]
+    sender_email = props["sender_email"]
+    sender_password = props["sender_password"]
+    subject = "Html report of pytest pipeline"
+    body = subject
+    emailto.send_report(subject, body, sender_email, sender_password, receiver_emails  )
